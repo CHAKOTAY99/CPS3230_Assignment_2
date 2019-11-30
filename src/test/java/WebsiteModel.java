1027c2cc2075465f8e0ee1416893fb5d2c595c84
Chakotay Incorvaja <chakotay.incorvaja@gmail.com>
2019-11-30 21:51:09 +0100

    Massive updates to LoggedInUser with ProductResultList

--- src/test/java/WebsiteModel.java
+++ src/test/java/WebsiteModel.java
@@ -36,9 +36,15 @@ public class WebsiteModel implements FsmModel {
 
     public void reset(boolean b) {
 
+        while(driver.findElements(By.className("div_ShoppingCartSummary_NoItemsMsg")).size() != 1){
+            driver.findElement(By.className("cell_ShoppingCartSummary_RemoveContainer")).click();
+            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        }
+
         if(driver.findElement(By.className("BannerSideLink")).getText().contains("Logout")){
             driver.findElement(By.className("BannerSideLink")).click();
         }
+
 
         modelState = WebsiteStates.LOGGED_OUT_USER;
         loggedOut = true;
@@ -55,11 +61,14 @@ public class WebsiteModel implements FsmModel {
 
 
     // Transitions including guards
-    public boolean loginUserGuard() { return getState().equals(WebsiteStates.LOGGED_OUT_USER); }
-
+    public boolean loginUserGuard() {
+        return systemUnderTest.isLoggedOut() && (getState().equals(WebsiteStates.LOGGED_OUT_USER) ||
+                getState().equals(WebsiteStates.RESULT_LIST) || getState().equals(WebsiteStates.SHOPPING_CART));
+    }
     @Action
     public void loginUser() {
         // Update the SUT
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         systemUnderTest.loggingIn();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
@@ -71,10 +80,13 @@ public class WebsiteModel implements FsmModel {
 
         // Checking correspondence between the model and SUT
         assertTrue("The model's user login state does not match the SUT's user login state", systemUnderTest.isLoggedIn());
-        assertFalse("The model's user logout state does not match te SUT's user logout state", systemUnderTest.isLoggedOut());
+        assertFalse("The model's user logout state does not match te SUT's user logout state", systemUnderTest.isLoggedOut() && systemUnderTest.isInResultsList() && systemUnderTest.isInHeadingResultsList() && systemUnderTest.isInShoppingCart());
     }
 
-    public boolean logoutUserGuard() { return getState().equals(WebsiteStates.LOGGED_IN_USER); }
+    public boolean logoutUserGuard() {
+        return systemUnderTest.isLoggedIn() && (getState().equals(WebsiteStates.LOGGED_IN_USER) || getState().equals(WebsiteStates.RESULT_LIST)
+        || getState().equals(WebsiteStates.SHOPPING_CART));
+    }
     @Action
     public void logoutUser() {
         // Update the SUT
@@ -91,7 +103,7 @@ public class WebsiteModel implements FsmModel {
         assertTrue("The model's user logout state does not match the SUT's user logout state", systemUnderTest.isLoggedOut());
     }
 
-    public boolean searchListGuard() { return getState().equals(WebsiteStates.LOGGED_OUT_USER) || getState().equals(WebsiteStates.LOGGED_IN_USER); }
+    public boolean searchListGuard() { return getState().equals(WebsiteStates.LOGGED_OUT_USER) || getState().equals(WebsiteStates.LOGGED_IN_USER) || getState().equals(WebsiteStates.SHOPPING_CART) || getState().equals(WebsiteStates.RESULT_LIST); }
     @Action
     public void searchList() {
         // Update the SUT
@@ -105,5 +117,108 @@ public class WebsiteModel implements FsmModel {
         // Checking correspondence between the model and the SUT
         assertTrue("The model's search state does not match the SUT's search state", systemUnderTest.isInResultsList());
         assertTrue("The model's user state does not match the SUT's user state", systemUnderTest.isLoggedOut() || systemUnderTest.isLoggedIn());
+        assertFalse("The model's state should not match the SUT's state", systemUnderTest.isInShoppingCart());
+    }
+
+    public boolean searchCategoryGuard() { return getState().equals(WebsiteStates.LOGGED_OUT_USER) || getState().equals(WebsiteStates.LOGGED_IN_USER) || getState().equals(WebsiteStates.SHOPPING_CART) || getState().equals(WebsiteStates.RESULT_LIST); }
+    @Action
+    public void searchCategory(){
+        // Update the SUT
+        systemUnderTest.searchCategory();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        // Update Model
+        modelState = WebsiteStates.RESULT_LIST;
+        resultList = true;
+
+        // Checking correspondence between the model and the SUT
+        assertTrue("The model's result list state does not match the SUT's result list state", systemUnderTest.isInResultsList());
+        assertTrue("The model's user state does not match the SUT's user state", systemUnderTest.isLoggedOut() || systemUnderTest.isLoggedIn());
+        assertFalse("The model's state should not match the SUT's state", systemUnderTest.isInShoppingCart() && systemUnderTest.isInHeadingResultsList());
+    }
+
+    public boolean searchProductHeading_SystemsGuard() { return getState().equals(WebsiteStates.LOGGED_OUT_USER) || getState().equals(WebsiteStates.LOGGED_IN_USER) || getState().equals(WebsiteStates.SHOPPING_CART) || getState().equals(WebsiteStates.RESULT_LIST); }
+    @Action
+    public void searchProductHeading_Systems() {
+        // Update the SUT
+        systemUnderTest.searchProductHeading(3);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        // Update Model
+        modelState = WebsiteStates.RESULT_LIST;
+        resultList = true;
+
+        // Checking correspondence between the model and the SUT
+        assertTrue("The model's result list state does not match the SUT's result list state", systemUnderTest.isInResultsList());
+        assertTrue("The model's user state does not match the SUT's user state", systemUnderTest.isLoggedOut() || systemUnderTest.isLoggedIn());
+        assertFalse("The model's state should not match the SUT's state", systemUnderTest.isInShoppingCart() && systemUnderTest.isInHeadingResultsList());
+    }
+
+    public boolean searchProductHeading_NotebookGuard() { return getState().equals(WebsiteStates.LOGGED_OUT_USER) || getState().equals(WebsiteStates.LOGGED_IN_USER) || getState().equals(WebsiteStates.SHOPPING_CART) || getState().equals(WebsiteStates.RESULT_LIST); }
+    @Action
+    public void searchProductHeading_Notebook(){
+        // Update the SUT
+        systemUnderTest.searchProductHeading(4);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        // Update Model
+        modelState = WebsiteStates.RESULT_LIST;
+        resultList = true;
+
+        // Checking correspondence between the model and the SUT
+        assertTrue("The model's result list state does not match the SUT's result list state", systemUnderTest.isInResultsList());
+        assertTrue("The model's user state does not match the SUT's user state", systemUnderTest.isLoggedOut() || systemUnderTest.isLoggedIn());
+        assertFalse("The model's state should not match the SUT's state", systemUnderTest.isInShoppingCart() && systemUnderTest.isInHeadingResultsList());
+    }
+
+    public boolean searchNewProductHeadingGuard() { return getState().equals(WebsiteStates.LOGGED_OUT_USER) || getState().equals(WebsiteStates.LOGGED_IN_USER) || getState().equals(WebsiteStates.SHOPPING_CART) || getState().equals(WebsiteStates.RESULT_LIST); }
+    @Action
+    public void searchNewProductHeading(){
+        // Update the SUT
+        systemUnderTest.searchProductHeading(1);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        // Update Model
+        modelState = WebsiteStates.RESULT_LIST;
+        resultList = true;
+
+        // Checking correspondence between the model and the SUT
+        assertTrue("The model's result list state does not match the SUT's result list state", systemUnderTest.isInHeadingResultsList());
+        assertTrue("The model's user state does not match the SUT's user state", systemUnderTest.isLoggedOut() || systemUnderTest.isLoggedIn());
+        assertFalse("The model's state should not match the SUT's state", systemUnderTest.isInResultsList() && systemUnderTest.isInShoppingCart());
+    }
+
+    public boolean buyProduct_FromResultsListGuard() { return getState().equals(WebsiteStates.RESULT_LIST); }
+    @Action
+    public void buyProduct_FromResultsList() {
+        // Update the SUT
+        systemUnderTest.buyFromList_ViewCart();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        // Update Model
+        modelState = WebsiteStates.SHOPPING_CART;
+        resultList = false;
+        shopCart = true;
+
+        // Check correspondence between the model and the SUT
+        assertTrue("The model's cart state does not match the SUT's cart state", systemUnderTest.isInShoppingCart());
+        assertFalse("The model's result list state does not match the SUT's result list state", systemUnderTest.isInResultsList());
+    }
+
+    public boolean buyProduct_FromResultsList_ModifyQtyGuard() { return getState().equals(WebsiteStates.RESULT_LIST); }
+    @Action
+    public void buyProduct_FromResultsList_ModifyQty() {
+        // Update the SUT
+        systemUnderTest.buyFromList_ViewCart_ModifyQty();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        // Update Model
+        modelState = WebsiteStates.SHOPPING_CART;
+        resultList = false;
+        shopCart = true;
+
+        // Check correspondence between the model and the SUT
+        assertTrue("The model's cart state does not match the SUT's cart state", systemUnderTest.isInShoppingCart());
+        assertFalse("The model's result list state does not match the SUT's result list state", systemUnderTest.isInResultsList());
     }
 }
--- src/test/java/pageObjects/SsSystem.java
+++ src/test/java/pageObjects/SsSystem.java
@@ -2,7 +2,9 @@ package pageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SsSystem {
@@ -34,19 +36,28 @@ public class SsSystem {
 
     public boolean isInRegistrationOrLogin(){ return regLogin; }
 
-    public boolean isInProductDetails(){ return prodDetails; }
+    public boolean isInProductDetails() { return prodDetails; }
 
-    public boolean isInResultsList(){
+    public boolean isInResultsList() {
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         return driver.findElement(By.className("PageTitle_Header1")).getText().contains("Showing");
     }
 
-    public boolean isInShoppingCart(){ return shopCart; }
+    public boolean isInHeadingResultsList() {
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        return driver.findElement(By.className("PageTitle_Header1")).getText().contains("New Products");
+    }
+
+
+    public boolean isInShoppingCart() {
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        return driver.findElement(By.className("PageTitle_Header1")).getText().contains("View Shopping Cart");
+    }
 
     public boolean isInCheckout(){ return checkOut; }
 
     public void loggingIn() {
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(By.className("BannerSideLink")).click();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.findElement(By.id("ctl00_MainContent_txt_UserName")).sendKeys("testingcps3230");
@@ -61,10 +72,41 @@ public class SsSystem {
         driver.findElement(By.className("BannerSideLink")).click();
     }
 
-    public void searchProduct(){
+    public void searchProduct() {
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.findElement(By.id("ctl00_txt_SearchBox")).clear();
         driver.findElement(By.id("ctl00_txt_SearchBox")).sendKeys("Eminent 6030 30m Patch Cat5E Grey");
         driver.findElement(By.id("ctl00_btn_Search")).click();
+    }
+
+    public void searchCategory() {
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.findElement(By.className("treeview_NodeImageCSS")).click();
+    }
+
+    public void searchProductHeading(int num) {
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        List<WebElement> productList = driver.findElements(By.className("HyperLink_MenuLink"));
+        // PC Systems (3) Notebooks (4). Always 5 headings. Offers are seasonal, ez. black friday
+        productList.get(num).click();
+    }
+
+    public void buyFromList_ViewCart(){
+        List<WebElement> productList = driver.findElements(By.xpath("//*[contains(@id,'btn_Buy')]"));
+        productList.get(0).click();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        driver.findElement(By.name("ctl00$ctl18$ctl00$ctl01")).click();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+    }
+
+    public void buyFromList_ViewCart_ModifyQty(){
+        List<WebElement> qtyList = driver.findElements(By.className("Products_TextBox_Quantity"));
+        qtyList.get(0).clear();
+        qtyList.get(0).sendKeys("2");
+        List<WebElement> productList = driver.findElements(By.xpath("//*[contains(@id,'btn_Buy')]"));
+        productList.get(0).click();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        driver.findElement(By.name("ctl00$ctl18$ctl00$ctl01")).click();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
     }
 }
